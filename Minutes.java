f34a9661c2cd13d0c30d0afd7c50016ed05ceaad
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-02-14 14:25:53 -0500

    a second one

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,24 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description 1.5.1 - 2
+  * @author:edwin_liu1
   */
   
   public void run() {
     
     // start coding here
-    
+    int intminutes;
+    int inthours;
+    int intdays;
+    int intremainder;
+
+    intminutes = readInt("Enter number of minutes: ");
+
+    intdays = intminutes / 1440;
+    inthours = (intminutes % 1440) / 60;
+    intremainder = (intminutes % 1440) % 60;
+
+    System.out.println(intminutes + " minutes = " + intdays + " days, " + inthours + " hours, and " + intremainder + " minutes.");
   }
 }
