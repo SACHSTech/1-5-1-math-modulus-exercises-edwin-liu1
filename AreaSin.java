fd5ca953f0d4ced1664306ebd146ea30d6d61dde
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-02-15 12:35:16 -0500

    Area is found

--- AreaSin.java
+++ AreaSin.java
@@ -1,13 +1,25 @@
 class AreaSin extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description 1.5.1 - 6
+  * @author:edwin_liu1
   */
   
   public void run() {
     
-    // start coding here
-    
+    double doubleSide1;
+    double doubleSide2;
+    double doubleAngle;
+    double doubleArea;
+
+    //reused code :3
+    doubleSide1 = readDouble("What is the first side of your triangle? ");
+    doubleSide2 = readDouble("What is the second side of your triangle? ");
+    doubleAngle = readDouble("What is the angle of your triangle? ");
+
+    doubleArea = (doubleSide1 * doubleSide2 *  Math.sin( doubleAngle) ) / 2;
+
+    System.out.println("The area of your triangle is " + doubleArea);
+
   }
 }
