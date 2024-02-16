e71082165d0255f2281492dca66ac0915fbd5b2b
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-02-16 13:43:34 -0500

    Powers. it works with non whole numbers

--- Power.java
+++ Power.java
@@ -1,13 +1,22 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description 1.5.1 - 9
+  * @author:edwin_liu1
   */
   
   public void run() {
     
-    // start coding here
+    double doubleNumber;
+    double doublePower;
+    double doubleEndNumber;
+
+    doubleNumber = readDouble("Enter a number you want powered: ");
+    doublePower = readDouble("Enter the power for the number: ");
+
+    doubleEndNumber = Math.pow(doubleNumber, doublePower);
+
+    System.out.println("your final number is " + doubleEndNumber);
     
   }
 }
